f960ca6e16d9bc95ba3280c096e40e5f7ca01d63
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-11 14:50:56 +0700

    change router

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ Router.get('/', (req,res,next) => {
     res.json({message: "We are connected"})
 })
 
+Router.get('/adding-new', (req,res,next) => {
+    res.json({message: "add new"})
+})
+
 module.exports = Router;
